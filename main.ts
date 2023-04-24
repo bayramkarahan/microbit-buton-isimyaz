91833dba1c73a944e9354d305abda66284059393
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2023-04-24 09:34:20 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # # # # .
+            # . . # .
+            # # # . .
+            `)
+        basic.pause(100)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            # . . # .
+            # . # . .
+            # # . . .
+            # . # . .
+            # . . # .
+            `)
+        basic.pause(100)
+    }
+})
